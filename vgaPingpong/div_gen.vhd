f715dc71ef96cc7d63c734981095e02c39c7da24
IsraMejia <ihmaisrael@gmail.com>
2023-01-03 00:02:32 -0600

    detalles divisor de frecuencias generico

--- vgaPingpong/div_gen.vhd
+++ vgaPingpong/div_gen.vhd
@@ -18,7 +18,7 @@ end div_gen;
 
 architecture div_gen_arch of div_gen is
 
-	signal Qt : integer range 0 to ((div/2)-1);
+	signal ContadorAscendente : integer range 0 to ((div/2)-1); --ContadorDeFlancosAscendentes
 	signal temp: std_logic;
 
 begin
@@ -28,18 +28,18 @@ begin
 	begin
 		
 		if(encendido = '0') then --encendido = apagado
-			Qt <= 0; --qt ayuda a que no sea un desastre todo , se pone super rapido el juego
+			ContadorAscendente <= 0; --contadorascendente ayuda a que no sea un desastre todo , se pone super rapido el juego
 			temp  <= '0'; --reloj de salida necesario 
 			
 		elsif(clk_in'event and clk_in = '1') then 
-		--Si tenemos el juego encendido y a su vez el estado del reloj esta en 1
-			
-			if(Qt = ((div/2)-1)) then 
-			  temp<= not temp;-- negamos el estado del reloj y lo guardamos en temporal
-			  	-- Al saber que la entrada es de 50MHz -> nos dara una señal de 25MHz
-			  Qt <= 0;
+		--Si tenemos el juego encendido y a su vez el estado del reloj esta en 1 (flanco ascendente)
+			if(ContadorAscendente = ((div/2)-1)) then 
+				--Cuando sea el momento de negar la señal de entrada para obtener 
+				--la frecuencia deseada
+			  temp<= not temp;-- negamos el estado del reloj temporal que seria de nuestra nueva frecuencia
+			  ContadorAscendente <= 0; --reiniciamos el contador de flancos ascendentes de la señal original
 			else
-			  Qt <= Qt+1;
+			  ContadorAscendente <= ContadorAscendente+1; --seguimos contando si un no es el momento de hacer lo anterior
 			end if;
 		end if;
 
