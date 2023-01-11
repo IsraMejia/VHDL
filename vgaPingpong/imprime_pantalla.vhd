274f5e8fd902bc2f915088982e0055e82bfe80e6
IsraMejia <ihmaisrael@gmail.com>
2023-01-10 22:48:15 -0600

    cambios recientes, probare hacer un proceso para cada raqueta

--- vgaPingpong/imprime_pantalla.vhd
+++ vgaPingpong/imprime_pantalla.vhd
@@ -7,7 +7,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity imprime_pantalla is
+entity imprime_pantalla is 
 
 	generic(
 	
@@ -413,12 +413,14 @@ begin
 
 
 
+ya hice los cambios, pero sigue sin funcionar, a continuacion te mando el codigo, para que lo puedas revisar :
+
 
 	
 	--Proceso que se encarga de dibujar el juego en la pantalla
 	process(coord_x_raqueta1, coord_y_raqueta1, 
 			coord_x_raqueta2, coord_y_raqueta2, 
-			habilitador, 
+			habilitador, reloj_raquetas,
 			contador_renglones, contador_columnas,
 			R1,G1,B1,	R2,G2,B2) begin
 		
@@ -428,56 +430,54 @@ begin
 				--En caso de que estemos en un area perteneciente al jugador 1
 				if ((coord_x_raqueta1 <= contador_columnas + MedidaHorizontalRaqueta) and
 					(coord_x_raqueta1 + MedidaHorizontalRaqueta >= contador_columnas) and
-					(coord_y_raqueta1 <= contador_renglones + MedidaVerticalRaqueta) and
-					(coord_y_raqueta1 + MedidaVerticalRaqueta >= contador_renglones)) --then
-						--if((R1 ='0') and (G1 ='0') and (B1 ='0')) then --Color por defecto verde retro :D
-							--R <= "1110";
-							--G <= "1110";
-							--B <= "0000";
-						--else 
-				--			if (R1 ='0') then R <= "0000"; else R <= "1111"; end if;                          
-				--			if (G1 ='0') then G <= "0000"; else G <= "1111"; end if;
-				--			if (B1 ='0') then B <= "0000"; else B <= "1111"; end if;
-						--end if; 
-				
-				--else	--Para el resto (fondo de la pantalla)			
-						--R <= "1000";
-						--G <= "0000";
-						--B <= "1100";				
-				--end if;
+					(coord_y_raqueta1 <= contador_renglones + MedidaVerticalRaqueta ) and
+					(coord_y_raqueta1 + MedidaVerticalRaqueta >= contador_renglones)) 	then
+						--R <= "1110";
+						--G <= "1110";
+						--B <= "0000";
 
-				--end if;
+					if (reloj_raquetas = '0' and reloj_raquetas'event and reloj_raquetas = '1')then
+						if((R1 ='0') and (G1 ='0') and (B1 ='0')) then --Color por defecto verde retro :D
+							R <= "1110";
+							G <= "1110";
+							B <= "0000";
+						else 
+							if (R1 ='0') then R <= "0000"; else R <= "1111"; end if;                          
+							if (G1 ='0') then G <= "0000"; else G <= "1111"; end if;
+							if (B1 ='0') then B <= "0000"; else B <= "1111"; end if;
+						end if; 
+					end if;
+				
+				end if;
 							
 					
 				--En caso de que estemos en un area perteneciente al jugador 2
-				--if  
-				or((coord_x_raqueta2 <= contador_columnas + MedidaHorizontalRaqueta) and
+				if ((coord_x_raqueta2 <= contador_columnas + MedidaHorizontalRaqueta) and
 					(coord_x_raqueta2 + MedidaHorizontalRaqueta >= contador_columnas) and
-					(coord_y_raqueta2 <= contador_renglones + MedidaVerticalRaqueta) and
-					(coord_y_raqueta2 + MedidaVerticalRaqueta >= contador_renglones)) --then 
-						--if((R2 ='0') and (G2 ='0') and (B2 ='0')) then --Color por defecto verde retro :D
-							--R <= "1110";
-							--G <= "1110";
-							--B <= "0000";
-						--else 
-						--	if (R2 ='0') then R <= "0000"; else R <= "1111"; end if;                      
-						--	if (G2 ='0') then G <= "0000"; else G <= "1111"; end if;
-						--	if (B2 ='0') then B <= "0000"; else B <= "1111"; end if; 
-						--end if; 
-				
-				--else	--Para el resto (fondo de la pantalla)			
-						--R <= "1000";
-						--G <= "0000";
-						--B <= "1100";				
-				--end if;
+					(coord_y_raqueta2 <= contador_renglones + MedidaVerticalRaqueta ) and
+					(coord_y_raqueta2 + MedidaVerticalRaqueta >= contador_renglones)) 	then 
+						--R <= "1110";
+						--G <= "1110";
+						--B <= "0000";
 
-				--end if;
+					if (reloj_raquetas = '0' and reloj_raquetas'event and reloj_raquetas = '1')then
+						if((R2 ='0') and (G2 ='0') and (B2 ='0')) then --Color por defecto verde retro :D
+							R <= "1110";
+							G <= "1110";
+							B <= "0000";
+						else 
+							if (R2 ='0') then R <= "0000"; else R <= "1111"; end if;                      
+							if (G2 ='0') then G <= "0000"; else G <= "1111"; end if;
+							if (B2 ='0') then B <= "0000"; else B <= "1111"; end if; 
+						end if; 
+					end if;
+				
+				end if;
 				
 					
 				--En caso de que estemos en un area perteneciente a la Pelota
-				--if	
-				or ((Coord_x_pelota <= contador_columnas + MedidaPelota) and
-					(Coord_x_PELOTA + MedidaPelota >= contador_columnas) and
+				if ((Coord_x_pelota <= contador_columnas + MedidaPelota ) and
+					(Coord_x_PELOTA + MedidaPelota >= contador_columnas ) and
 					(Coord_y_pelota <= contador_renglones + MedidaPelota) and
 					(Coord_y_pelota + MedidaPelota >= contador_renglones))	then				
 					--Color por defecto verde retro :D
@@ -503,4 +503,5 @@ begin
 	end process;
 
 end imprime_pantalla_bhv;
-			
+
+
